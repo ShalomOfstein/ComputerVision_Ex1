fef803fcbd70efe1f6cab16e7c6a86e9659a5034
Shalom Ofstein <Shalomofstein@gmail.com>
2024-06-06 10:45:08 +0300

    Initial commit

---
+++ ex1_utils.py
@@ -0,0 +1,101 @@
+"""
+        '########:'##::::'##::::'##:::
+         ##.....::. ##::'##:::'####:::
+         ##::::::::. ##'##::::.. ##:::
+         ######:::::. ###::::::: ##:::
+         ##...:::::: ## ##:::::: ##:::
+         ##:::::::: ##:. ##::::: ##:::
+         ########: ##:::. ##::'######:
+        ........::..:::::..:::......::
+"""
+from typing import List
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+LOAD_GRAY_SCALE = 1
+LOAD_RGB = 2
+
+
+def myID() -> np.int:
+
+    return 318845500
+
+
+def imReadAndConvert(filename: str, representation: int) -> np.ndarray:
+    """
+    Reads an image, and returns the image converted as requested
+    :param filename: The path to the image
+    :param representation: GRAY_SCALE or RGB
+    :return: The image object
+    """
+    image = cv2.imread(filename)
+    if representation == 1:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    if representation == 2:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Convert image to float and normalize to range [0, 1]
+    image = image.astype(np.float32) / 255.0
+
+    return image
+
+
+def imDisplay(filename: str, representation: int):
+    """
+    Reads an image as RGB or GRAY_SCALE and displays it
+    :param filename: The path to the image
+    :param representation: GRAY_SCALE or RGB
+    :return: None
+    """
+    # Convert the image using the imReadAndConvert function
+    image = imReadAndConvert(filename, representation)
+    # Display the image using plt.imshow
+    plt.figure()
+
+    if representation == 1:
+        plt.imshow(image, cmap ='gray')
+    else:
+        plt.imshow(image)
+
+    plt.axis('off')  # Hide axes
+    plt.show()
+
+
+
+def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
+    """
+    Converts an RGB image to YIQ color space
+    :param imgRGB: An Image in RGB
+    :return: A YIQ in image color space
+    """
+    pass
+
+
+def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
+    """
+    Converts an YIQ image to RGB color space
+    :param imgYIQ: An Image in YIQ
+    :return: A RGB in image color space
+    """
+    pass
+
+
+def hsitogramEqualize(imgOrig: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarray):
+    """
+        Equalizes the histogram of an image
+        :param imgOrig: Original Histogram
+        :ret
+    """
+    pass
+
+
+def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarray], List[float]):
+    """
+        Quantized an image in to **nQuant** colors
+        :param imOrig: The original image (RGB or Gray scale)
+        :param nQuant: Number of colors to quantize the image to
+        :param nIter: Number of optimization loops
+        :return: (List[qImage_i],List[error_i])
+    """
+    pass
